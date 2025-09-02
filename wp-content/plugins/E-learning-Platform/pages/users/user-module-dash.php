6f7bc4098a43d68c8590a23e270eec3e46b96baf
Rory <FireHat069@gmail.com>
2025-09-03 09:47:51 +1000

    quickly added the theme to a page as a test for Barry

--- wp-content/plugins/E-learning-Platform/pages/users/user-module-dash.php
+++ wp-content/plugins/E-learning-Platform/pages/users/user-module-dash.php
@@ -2,9 +2,12 @@
 if ( ! defined( 'ABSPATH' ) ) {
     exit;
 }
+get_header();
 ?>
 <div class="elearn-dashboard">
     <h2>Welcome to Your E-Learning Dashboard</h2>
     <a href="<?php echo home_url('?elearn_page=modules'); ?>" class="button">View Modules</a>
     <a href="<?php echo home_url('?elearn_page=assessment-results'); ?>" class="button">View Results</a>
 </div>
+<?php
+get_footer();
